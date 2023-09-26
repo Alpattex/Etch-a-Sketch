064f3203f284ef8c3ee43532e868196714117f6b
odinstudent <34312554+Alpattex@users.noreply.github>
2023-09-26 13:51:15 +0300

    Resetting Repository to Initial State

--- script.js
+++
@@ -1,9 +0,0 @@
-const gridContainer = document.getElementById('grid-container');
-
-for (let row = 0; row < 16; row++) {
-    for (let col = 0; col < 16; col++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        gridContainer.appendChild(gridItem);
-    }
-}
